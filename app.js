e6682df7e54a7a3885998cc0176a9eb456ab0441
Nazim Filzer <nazimfilzer@gmail.com>
2022-07-07 21:05:42 +0530

    PORT

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ app.post("/contact", async function (req, res) {
 
 
 
-app.listen(3000 || process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
